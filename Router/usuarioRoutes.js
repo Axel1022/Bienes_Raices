34304da3402de75fb7a1e8e6da8946cca1268de7
Gary Alexander Campusano Paredes <ingcampusano.outlook.com>
2024-12-10 17:45:00 -0400

    GC -He agregado la ruta para recuperar cuenta

--- Router/usuarioRoutes.js
+++ Router/usuarioRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 import {
   formularioLogin,
   formularioRegistro,
+  formularioOlvidPass,
 } from "../Controllers/usuarioController.js";
 
 router.get("/login", formularioLogin);
 router.get("/registro", formularioRegistro);
+router.get("/olvide-pass", formularioOlvidPass);
 
 export default router;
