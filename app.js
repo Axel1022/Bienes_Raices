da50530c07caab0454d459c4df5dd3ce947fe10f
Gary Alexander Campusano Paredes <ingcampusano.outlook.com>
2024-12-11 11:09:13 -0400

    GC - Cambie index por app

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import usuarioRoutes from "./Router/usuarioRoutes.js";
+import NotFund from "./Controllers/404Controller.js";
+
+const app = express();
+
+// Configuración
+app.set("view engine", "pug");
+app.set("views", "./Views");
+app.use(express.static("public"));
+
+// Rutas
+app.use("/auth", usuarioRoutes);
+app.use("/", NotFund);
+
+// Middleware para manejar errores 404
+app.use((req, res, next) => {
+  res.status(404).render("404", { title: "Página no encontrada" });
+});
+
+// Middleware para manejar errores generales
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).render("500", { title: "Error del servidor" });
+});
+
+export default app;
