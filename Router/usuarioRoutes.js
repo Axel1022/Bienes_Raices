e80bcceb02b9ffd1a9b2a8a6a0b09e1f51fcf2ce
Gary Alexander Campusano Paredes <ingcampusano.outlook.com>
2024-12-11 19:06:47 -0400

    GC - Ruta para registar un usuario

--- Router/usuarioRoutes.js
+++ Router/usuarioRoutes.js
@@ -4,10 +4,12 @@ import {
   formularioLogin,
   formularioRegistro,
   formularioOlvidPass,
+  registrar,
 } from "../Controllers/usuarioController.js";
 
 router.get("/login", formularioLogin);
 router.get("/registro", formularioRegistro);
+router.post("/registro", registrar);
 router.get("/olvide-pass", formularioOlvidPass);
 
 export default router;
