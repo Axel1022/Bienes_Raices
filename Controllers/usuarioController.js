db5764a83a7e906e07f2ba610f04f0a0f0fb9c9e
Gary Alexander Campusano Paredes <ingcampusano.outlook.com>
2024-12-12 00:21:42 -0400

    GC - modified

--- Controllers/usuarioController.js
+++ Controllers/usuarioController.js
@@ -1,17 +1,63 @@
+import { check, validationResult } from "express-validator";
+import Usuario from "../Models/Usuario.js";
+
 const formularioLogin = async (req, res) => {
-  res.render("Auth/login", {
+  res.render("auth/login", {
     pagina: "Iniciar Sesión",
   });
 };
 const formularioRegistro = async (req, res) => {
-  res.render("Auth/registro", {
+  res.render("auth/registro", {
     pagina: "Crear Cuenta",
   });
 };
-const registrar = async (req, res) => {
-  res.render("Auth/registro", {
-    pagina: "Crear Cuenta",
+const registrar = async (req, res, next) => {
+  //Validaciones
+  await check("nombre", "El nombre es obligatorio.").notEmpty().run(req);
+  await check("correo", "El correo es obligatorio.").notEmpty().run(req);
+  await check("correo", "Debe ser un correo válido.").isEmail().run(req);
+  await check("contrasena", "La contraseña debe tener al menos 8 caracteres.")
+    .isLength({ min: 8 })
+    .run(req);
+  await check("Ccontrasena", "Las contraseñas deben coincidir.")
+    .custom((value, { req }) => value === req.body.contrasena)
+    .run(req);
+
+  let resultado = validationResult(req);
+
+  if (!resultado.isEmpty()) {
+    return res.render("auth/registro", {
+      pagina: "Crear Cuenta",
+      errores: resultado.array(),
+      usuario: {
+        nombre: req.body.nombre,
+        correo: req.body.correo,
+        contrasena: req.body.contrasena,
+        Ccontrasena: req.body.Ccontrasena,
+      },
+    });
+  }
+  const existeEmail = await Usuario.findOne({
+    where: { email: req.body.correo },
   });
+  if (existeEmail) {
+    return res.render("auth/registro", {
+      pagina: "Crear Cuenta",
+      errores: [{ msg: "Este correo ya está en uso." }],
+      usuario: {
+        nombre: req.body.nombre,
+        correo: req.body.correo,
+      },
+    });
+  }
+  await Usuario.create({
+    nombre: req.body.nombre,
+    email: req.body.correo,
+    password: req.body.contrasena,
+    token: "-",
+    confirmado: false,
+  });
+  res.redirect("/auth/login");
 };
 const formularioOlvidPass = async (req, res) => {};
 
