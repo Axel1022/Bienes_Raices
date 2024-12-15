49b339594da1ec985bd43c817fae873acd94ffd5
Gary Alexander Campusano Paredes <ingcampusano.outlook.com>
2024-12-15 01:35:00 -0400

    GC - Ruta para confirmar usuario con token

--- Router/usuarioRoutes.js
+++ Router/usuarioRoutes.js
@@ -5,11 +5,13 @@ import {
   formularioRegistro,
   formularioOlvidPass,
   registrar,
+  confirmar,
 } from "../Controllers/usuarioController.js";
 
 router.get("/login", formularioLogin);
 router.get("/registro", formularioRegistro);
 router.post("/registro", registrar);
+router.get("/confirmar/:token", confirmar);
 router.get("/olvide-pass", formularioOlvidPass);
 
 export default router;
