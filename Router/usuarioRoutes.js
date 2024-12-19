d14ad9281e9139c96f831ffd630a73bd04ec1f30
Gary Alexander Campusano Paredes <ingcampusano.outlook.com>
2024-12-18 20:44:15 -0400

    GC - Rutas para cambiar la contrasena

--- Router/usuarioRoutes.js
+++ Router/usuarioRoutes.js
@@ -3,15 +3,30 @@ const router = express.Router();
 import {
   formularioLogin,
   formularioRegistro,
-  formularioOlvidPass,
-  registrar,
-  confirmar,
+  formularioVerificarCorreo,
+  validarPass,
+  validarRegistro,
+  confirmarUsuario,
+  FormularioNuevaPass,
+  validarNuevaPass,
 } from "../Controllers/usuarioController.js";
 
-router.get("/login", formularioLogin);
-router.get("/registro", formularioRegistro);
-router.post("/registro", registrar);
-router.get("/confirmar/:token", confirmar);
-router.get("/olvide-pass", formularioOlvidPass);
+// Rutas públicas
+router.get("/login", formularioLogin); // Formulario para el login
+router.get("/registro", formularioRegistro); // Formulario para el registro de usuario
+
+// Registro de usuario
+router.post("/registro", validarRegistro); // Lógica para registrar un usuario
+
+// Confirmación de token para activar al usuario
+router.get("/confirmar/:token", confirmarUsuario); // Confirmación de usuario mediante un token único
+
+// Rutas para recuperar la contraseña
+router.get("/olvide-pass", formularioVerificarCorreo); // Muestra el formulario para ingresar el correo del usuario
+router.post("/olvide-pass", validarPass); // Si el correo existe, se envía el token al correo
+
+// Rutas para cambiar la contraseña (requiere un token válido)
+router.get("/cambiar-pass/:token", FormularioNuevaPass); // Muestra el formulario para cambiar la contraseña
+router.post("/cambiar-pass/:token", validarNuevaPass); // Valida la nueva contraseña, y elimina el token para evitar reutilización
 
 export default router;
