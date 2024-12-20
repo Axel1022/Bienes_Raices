d34ab8ff5056821ff9b7a7959245168a39a8450d
Gary Alexander Campusano Paredes <ingcampusano.outlook.com>
2024-12-20 03:31:39 -0400

    GC - Controller para mostrar formulario para crear propiedad

--- Controllers/propiedadesController.js
+++ Controllers/propiedadesController.js
@@ -1,8 +1,16 @@
-const propiedades = (req, res) => {
+//Muestras las propiedades en el home
+const homePropiedades = (req, res) => {
   res.render("propiedades/admin", {
     pagina: "Mis Propiedades",
     navBar: true,
   });
 };
+//Formulario para crear las propiedades
+const crearPropiedad = (req, res) => {
+  res.render("propiedades/crearPropiedad", {
+    pagina: "Crear Propiedad",
+    navBar: true,
+  });
+};
 
-export { propiedades };
+export { homePropiedades, crearPropiedad };
