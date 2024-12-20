361d6f057be4ba6c1c824ca723ebd9efd1b68090
Gary Alexander Campusano Paredes <ingcampusano.outlook.com>
2024-12-20 19:27:01 -0400

    GC - Eliminar datos

--- seed/seeder.js
+++ seed/seeder.js
@@ -3,6 +3,7 @@ import Categoria from "../Models/Categoria.js";
 import precios from "./precios.js";
 import Precio from "../Models/Precio.js";
 import db from "../config/db.js";
+import { where } from "sequelize";
 
 const importarDatos = async () => {
   try {
@@ -23,6 +24,20 @@ const importarDatos = async () => {
     process.exit(1);
   }
 };
+const eliminarDatos = async () => {
+  try {
+    await Promise.all([
+      Categoria.destroy({ where: {}, truncate: true }),
+      Precio.destroy({ where: {}, truncate: true }),
+    ]);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 if (process.argv[2] === "-i") {
   importarDatos();
+}
+if (process.argv[2] === "-e") {
+  eliminarDatos();
 }
