4044a73800d230e18c7aaff3d531236df8bbd3ed
Gary Alexander Campusano Paredes <ingcampusano.outlook.com>
2024-12-20 21:35:01 -0400

    GC - Cargando la categoria y precio

--- Controllers/propiedadesController.js
+++ Controllers/propiedadesController.js
@@ -1,3 +1,6 @@
+import CategoriaModel from "../Models/Categoria.js";
+import PrecioModel from "../Models/Precio.js";
+
 //Muestras las propiedades en el home
 const homePropiedades = (req, res) => {
   res.render("propiedades/admin", {
@@ -6,10 +9,27 @@ const homePropiedades = (req, res) => {
   });
 };
 //Formulario para crear las propiedades
-const crearPropiedad = (req, res) => {
+const crearPropiedad = async (req, res) => {
+  const [categorias, precios] = await Promise.all([
+    CategoriaModel.findAll(),
+    PrecioModel.findAll(),
+  ]);
+
+  //Devuelve los datos mapeados (solos los que estan en dataValues)
+  // Verifica si hay datos... ( dato.length ? proceso : [])
+  const categoriasMap = categorias.length
+    ? categorias.map(({ dataValues }) => dataValues)
+    : [];
+  const preciosMap = precios.length
+    ? precios.map(({ dataValues }) => dataValues)
+    : [];
+
+  console.log(categoriasMap);
   res.render("propiedades/crearPropiedad", {
     pagina: "Crear Propiedad",
     navBar: true,
+    Categorias: categoriasMap,
+    Precios: preciosMap,
   });
 };
 
