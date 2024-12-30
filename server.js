89d25d2a89451cb2439f29e97207310280f5bfb1
Gary Alexander Campusano Paredes <ingcampusano.outlook.com>
2024-12-30 05:24:49 -0400

    GC - Configuracion del servidor

--- server.js
+++ server.js
@@ -1,23 +1,38 @@
 import app from "./app.js";
 import connectiondb from "./config/db.js";
+import chalk from "chalk";
 
-const port = process.env.PORT || 8080;
+const port = parseInt(process.env.PORT, 10) || 8080;
+
+if (isNaN(port)) {
+  console.error("❌ El puerto especificado no es válido.");
+  process.exit(1);
+}
 
 const startServer = async () => {
   try {
     await connectiondb.authenticate();
-    console.log("✅ Conexión a la base de datos exitosa");
+    console.log(chalk.green("✅ Conexión a la base de datos exitosa"));
 
-    await connectiondb.sync();
-    console.log("✅ Base de datos sincronizada");
+    await connectiondb.sync({ alter: false });
+    console.log(chalk.green("✅ Base de datos sincronizada"));
 
     app.listen(port, () => {
       console.log(
-        `🚀 Servidor iniciado en http://localhost:${port}/auth/login`
+        chalk.blue(
+          `🚀 Servidor iniciado en http://localhost:${port}/auth/login`
+        )
       );
     });
+
+    process.on("SIGINT", async () => {
+      console.log("🔄 Cerrando el servidor...");
+      await connectiondb.close();
+      console.log(chalk.green("✅ Conexión a la base de datos cerrada"));
+      process.exit(0);
+    });
   } catch (error) {
-    console.error("❌ Error al iniciar el servidor:", error);
+    console.error(chalk.red("❌ Error al iniciar el servidor:"), error.message);
     process.exit(1);
   }
 };
