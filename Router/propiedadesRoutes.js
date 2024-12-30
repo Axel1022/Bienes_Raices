1198923b8a3c939d6c52c58df88801e0a0330b06
Gary Alexander Campusano Paredes <ingcampusano.outlook.com>
2024-12-30 05:06:52 -0400

    GC - Validando datos de propiedad

--- Router/propiedadesRoutes.js
+++ Router/propiedadesRoutes.js
@@ -1,15 +1,38 @@
 import express from "express";
+import { body } from "express-validator";
 const router = express.Router();
 
 // Importar controladores
+import { homePropiedades } from "../Controllers/Propiedades/homePropiedades.js";
+
 import {
-  homePropiedades,
-  crearPropiedad,
-} from "../Controllers/propiedadesController.js";
+  buscarCategoriasyPrecios,
+  mapearCategoriasyPrecios,
+  mostrarFormulario,
+} from "../Controllers/Propiedades/formularioPropiedad.js";
+
+import { validarDatos } from "../Controllers/Propiedades/crearPropiedad.js";
 
 //Rutas
-
 router.get("/propiedades", homePropiedades);
-router.get("/propiedades/crear", crearPropiedad);
+router.get(
+  "/propiedades/crear",
+  buscarCategoriasyPrecios,
+  mapearCategoriasyPrecios,
+  mostrarFormulario
+);
+router.post(
+  "/propiedades/crear",
+  body("titulo", "El título no puede estar vacío").notEmpty(),
+  body("descripcion", "La descripción no puede estar vacía").notEmpty(),
+  body("categoria", "La categoría es obligatoria").notEmpty(),
+  body("precio", "El precio es obligatorio").notEmpty(),
+  body("habitaciones", "El número de habitaciones es obligatorio").notEmpty(),
+  body("parqueos", "El número de parqueos es obligatorio").notEmpty(),
+  body("banos", "El número de baños no puede estar vacío").notEmpty(),
+  body("calle", "Ubica la propiedad en el mapa.").notEmpty(),
+  validarDatos
+);
+
 //Exportar rutas
 export default router;
