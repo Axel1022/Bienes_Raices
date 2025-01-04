c10b0c1e39e56c057d692cc0d067f24a60eb9d9e
Gary Alexander Campusano Paredes <ingcampusano.outlook.com>
2025-01-03 20:34:33 -0400

    GC - DELETED

--- Controllers/Propiedades/homePropiedades.js
+++
@@ -1,9 +0,0 @@
-//Muestras las propiedades en el home
-const homePropiedades = (req, res) => {
-  res.render("propiedades/admin", {
-    pagina: "Mis Propiedades",
-    navBar: true,
-  });
-};
-
-export { homePropiedades };
